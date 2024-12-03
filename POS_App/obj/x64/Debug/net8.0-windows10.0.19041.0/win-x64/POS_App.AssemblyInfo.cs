31355c193b41f66c1883e822f5724b574a6a0a47
Nguyễn Minh Khang <mkhang040506@gmail.com>
2024-12-03 10:12:37 -0800

    change layout of OrderPage

--- POS_App/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/POS_App.AssemblyInfo.cs
+++ POS_App/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/POS_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c959b50c5c03c31294f108a9271fbf0c46c63c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6eac3907de317c47ec087f0a552e6341bacbffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
